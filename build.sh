7c16714f7cb9189f3be3779bdbbffaca5178f8d1
Sean C. Sullivan <github@seansullivan.com>
2019-08-27 16:23:25 -0700

    update scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
-rm -rf ./noto-emoji
-git clone --depth 1 https://github.com/googlefonts/noto-emoji.git
-cd ./noto-emoji/svg && ../../svg2png.sh
+SVG_REPO_DIR=./noto-emoji
+if [ ! -d ${SVG_REPO_DIR} ]
+then
+  git clone --depth 1 https://github.com/googlefonts/noto-emoji.git
+fi
+image_sizes=(800)
+for size in ${image_sizes}; do
+  ./svg2png.sh ${size} ${SVG_REPO_DIR}/svg ./docs/assets/${size}
+done
