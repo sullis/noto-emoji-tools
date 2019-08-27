7fc71b3ee84cdd5826658fa7a5177e1b0dd013c7
Sean C. Sullivan <github@seansullivan.com>
2019-08-27 16:34:55 -0700

    image 400

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ ! -d ${SVG_REPO_DIR} ]
 then
   git clone --depth 1 https://github.com/googlefonts/noto-emoji.git
 fi
-image_sizes=(800)
+image_sizes=(400)
 for size in ${image_sizes}; do
   ./svg2png.sh ${size} ${SVG_REPO_DIR}/svg ./docs/assets/${size}
 done
